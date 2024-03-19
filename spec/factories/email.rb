28fcf6269c0fa23e66a96b7ac275b943c5b0d9cb
Matus Kmit <matuskmit1@gmail.com>
2024-03-19 16:19:51 +0100

    dl: produce periodic emails

--- spec/factories/email.rb
+++ spec/factories/email.rb
@@ -1,24 +1,24 @@
-FactoryBot.define do
+FactoryBot.modify do
   factory :email do
     subject { Faker::Lorem.sentence }
     body { Faker::Lorem.paragraph }
     from_address { Faker::Internet.email }
     to_address { user.email }
     user
-  end
 
-  trait :unsent do
-    trials { 0 }
-  end
+    trait :unsent do
+      trials { 0 }
+    end
 
-  trait :succeeded do
-    trials { 1 }
-    is_successful { true }
-  end
+    trait :succeeded do
+      trials { 1 }
+      is_successful { true }
+    end
 
-  trait :failed do
-    trials { 1 }
-    is_successful { false }
-    error_message { 'service unavailable' }
+    trait :failed do
+      trials { 1 }
+      is_successful { false }
+      error_message { 'service unavailable' }
+    end
   end
 end
